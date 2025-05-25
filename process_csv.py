7be7e9f2d4ee04ef17046dd883d97d4ba590ea84
Glen <glen00708@gmail.com>
2025-05-25 10:48:14 +0800

    separate test dir and train dir

--- process_csv.py
+++ process_csv.py
@@ -1,10 +1,13 @@
 import csv
 import pandas as pd
+import os
 
-filepath_test = "/Users/glenchua/Documents/thesis_project/Database/testdata_basic.csv"
-filepath_train = "/Users/glenchua/Documents/thesis_project/Database/trainingdata_basic.csv"
-rf_test = "Database/basic/Image/aligned/test_"
-rf_train = "Database/basic/Image/aligned/train_"
+BASE_URL = "/home/glen/projects/thesis_project"
+filepath_test = os.path.join(BASE_URL, "Database/testdata_basic.csv")
+filepath_train = os.path.join(BASE_URL, "Database/trainingdata_basic.csv")
+rf_test = "Database/basic/Image/aligned/test/test_"
+rf_train = "Database/basic/Image/aligned/train/train_"
+
 # train_data = []
 # test_data = []
 
@@ -42,13 +45,25 @@ def get_image_path(i):
         return f"{rf_test}0{i}_aligned.jpg"
     else:  
         return f"{rf_test}{i}_aligned.jpg"
-    # else:
-    #     return f"{rf_test}{i}_aligned.jpg"
+    
+
+def get_image_path_train(i):
+    if i < 10:
+        return f"{rf_train}0000{i}_aligned.jpg"
+    elif 10 <= i < 100:
+        return f"{rf_train}000{i}_aligned.jpg"
+    elif 100 <= i < 1000:
+        return f"{rf_train}00{i}_aligned.jpg"
+    elif 1000 <= i < 10000:  
+        return f"{rf_train}0{i}_aligned.jpg"
+    else:
+        return f"{rf_train}{i}_aligned.jpg"
 
 
-df = pd.read_csv(filepath_test)
-#df['Stress?'] = df['Label'].apply(infer_stress)
-new_filenames = [get_image_path(i) for i in range(1, len(df['Images']) + 1)]
-print(df.head())
-# df['Images'] = new_filenames
-df.to_csv('testdata_basic.csv')
+df = pd.read_csv(filepath_train)
+#df['Images'] = df['Label'].apply(infer_stress)
+new_filenames = [get_image_path_train(i) for i in range(1, len(df['Images']) + 1)]
+
+df['Images'] = new_filenames
+#print(df.head())
+df.to_csv('trainingdata_basic.csv')
